e87c4249d64359963e5a1e3764f74294b4af25fe
Programming-Ghost <ahmadayeham280@gmail.com>
2025-06-21 01:49:46 +0300

    final game

---
+++ 912/Assets/Mousecamera.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mousecamera : MonoBehaviour
+{
+    float mouseX;
+    float mouseY;
+    public float mouseSensitivity = 100f;
+
+
+    public Transform playerBody;
+
+
+    float rotation = 0f;
+
+    public float minAngle = -90f;
+    public float maxAngle = 90f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false; // Hide the cursor
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
+
+
+        rotation -= mouseY;
+      rotation =Mathf.Clamp(rotation, minAngle, maxAngle);
+        transform.localRotation = Quaternion.Euler(rotation, 0, 0);
+
+
+         playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
